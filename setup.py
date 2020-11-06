77c9dcfc9ca68adb1a2a9edc5d548de8fb260623
MarkBaggett <lo127001@gmail.com>
2020-11-06 05:18:52 -0800

    Update Version. Add description.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 import setuptools
 
-#with open("README.md", "r") as fh:
-#    long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="domain_stats", 
-    version="0.0.1",
+    version="0.0.8",
     author="MarkBaggett",
     author_email="lo127001@gmail.com",
     description="Malicious Domain Detection base on domain creation and first contact",
-    #long_description=long_description,
-    #long_description_content_type="text/markdown",
-    #scripts = ['domain_stats/domain_stats', "domain_stats/domain_stats_db_admin"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/markbaggett/domain_stats",
     packages=setuptools.find_packages(),
     install_requires = ['requests','pyyaml', 'rdap', 'python-dateutil>2.7','publicsuffixlist','flask','gunicorn', 'diskcache'],
