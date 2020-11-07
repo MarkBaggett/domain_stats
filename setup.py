ec99df8b9a91152efa84d4a45b5a35e5a7617ed6
MarkBaggett <lo127001@gmail.com>
2020-11-07 09:58:43 -0800

    Update Version Number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="domain_stats", 
-    version="0.0.8",
+    version="0.0.9",
     author="MarkBaggett",
     author_email="lo127001@gmail.com",
     description="Malicious Domain Detection base on domain creation and first contact",
