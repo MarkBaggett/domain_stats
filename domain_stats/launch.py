34d803324712f55fcc6ecbb42c4d914420bb3710
markbaggett <lo127001@gmail.com>
2021-04-22 06:40:06 -0700

    Fix relative paths at launch of process

--- domain_stats/launch.py
+++ domain_stats/launch.py
@@ -8,6 +8,7 @@ from domain_stats.settings import setup_directory
 def launch_from_config(tgt_folder):
     os.chdir(tgt_folder)
     launch_cmd = f"""{sys.executable} -m gunicorn.app.wsgiapp 'domain_stats.server:config_app("{tgt_folder}")' -c {tgt_folder}/gunicorn_config.py"""
+    print(f"Running {launch_cmd}")
     os.system(launch_cmd)
 
 def main():
@@ -17,12 +18,14 @@ def main():
         typed_folder = sys.argv[1]
 
     tgt_folder = pathlib.Path(typed_folder)
+    if typed_folder.startswith("."):
+        tgt_folder = pathlib.Path().cwd() / tgt_folder
     if not tgt_folder.is_dir():
         print("That directory does not exist. Please create it and/or try again.")
         sys.exit(1)
     if (tgt_folder / "domain_stats.yaml").is_file():
         print("Existing config found in directory. Using it.")
-        launch_from_config(tgt_folder)
+        launch_from_config(str(tgt_folder))
     elif input("Would you like to setup this directory now?").lower().startswith("y"):
         setup_directory(tgt_folder)
 
